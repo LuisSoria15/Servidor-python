f04f29331343300e18bd7435615e5b1eb692aa98
MontMG <256658966+MontMG@users.noreply.github.com>
2026-04-24 12:48:39 -0600

    Get opciones
    Se agrego el fragmento de codigo para que se muestren todas las opciones

--- server.py
+++ server.py
@@ -27,3 +27,20 @@ def obtener_todas_las_categorias():
         if conexion and conexion.is_connected():
             cursor.close()
             conexion.close()
+
+@app.get("/opciones")
+def obtener_todas_las_opciones():
+    conexion = None
+    try:
+        conexion = mysql.connector.connect(**DB_CONFIG)
+        cursor = conexion.cursor(dictionary=True)
+        
+        cursor.execute("SELECT id, categoria_id, enunciado, formato FROM opciones ORDER BY id ASC")
+        return cursor.fetchall()
+        
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        if conexion and conexion.is_connected():
+            cursor.close()
+            conexion.close()
