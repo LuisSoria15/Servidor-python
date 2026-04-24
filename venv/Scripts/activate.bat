84f574192e4e381d783e37a10898e575bea33440
MontMG <256658966+MontMG@users.noreply.github.com>
2026-04-24 12:21:43 -0600

    Eliminación de venv y descarga de requirements
    Se elimino la carpeta venv porque al jalar el proyecto de github había problemas

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\soria\OneDrive\Escritorio\Personal\UNI\Sem 7\Interfaces Graficas\Parcial3\Servidor\venv
+set VIRTUAL_ENV=C:\Users\katim\OneDrive\Documentos\GitHub\Servidor-python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
